f6ca19c54bec01d562cff54ad795b9c97d83629e
clayton14 <claytoneasley77@gmail.com>
2025-05-22 20:12:33 -0400

    Add week2
      Add project folder for week2

---
+++ week2/src/main.c
@@ -0,0 +1,18 @@
+/***************************************
+ * HOMEWORK:    <Assignment Number>
+ * 
+ * NAME:        Clayton Easley
+ * 
+ * DATE:        <YYYY-MM-DD>
+ * 
+ * DESCRIPTION: <description>
+ ****************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello World");
+    return 0;
+}
+
