20b20d8d42881ed68218669ab9658aad4f47fa99
clayton14 <claytoneasley77@gmail.com>
2025-05-27 15:00:05 -0400

    starting assignment 1
      Changed CMakeLists

--- week2/src/main.c
+++ week2/src/main.c
@@ -11,10 +11,33 @@
  ****************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+//FILE *output_ptr; // pointer to output file
 
 int main()
 {
-    puts("Hello World");
+
+    int clockNumber; // employee clock number 
+    float gross;     // gross pay for week (wage * hours) 
+    float hours;     // number of hours worked per week 
+    float wageRate;  // hourly wage 
+
+
+    printf("\n***** Pay Caculator *****\n");
+    printf("\nEnter Your Employee ID:\n");
+    scanf("%06d", &clockNumber);
+    if(clockNumber < 0)
+    {
+        printf("[ERROR] your Employee ID cannot be negative");
+        return 0;
+    }
+    printf("%06d", clockNumber);
+    printf("\nEnter hours worked:\n");
+    scanf("%f", &hours);
+    printf("%2.2f\n", hours);    
     return 0;
-}
+} // main
 
