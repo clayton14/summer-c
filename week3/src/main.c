7cf4e451a3e84b8b6c3d6c3370d9801bd524d5c6
clayton14 <claytoneasley77@gmail.com>
2025-06-02 13:20:53 -0400

    Start week3
      copy week1 template to week3

---
+++ week3/src/main.c
@@ -0,0 +1,20 @@
+/***************************************
+ * HOMEWORK:    <Assignment Number>
+ * 
+ * NAME:        Clayton Easley
+ * 
+ * CLASS:       C Programming, Summer 2025
+ *  
+ * DATE:        <YYYY-MM-DD>
+ * 
+ * DESCRIPTION: <description>
+ ****************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello World");
+    return 0;
+}
+
