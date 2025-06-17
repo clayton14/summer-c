66b9e53b25d7d4392f6ab56c86e37ce649877316
clayton14 <claytoneasley77@gmail.com>
2025-06-17 12:19:59 -0400

    Use Arrays to hold data
      Week 5 work is in week4 folder... oops

--- week4/src/main.c
+++ week4/src/main.c
@@ -9,23 +9,27 @@
  *
  * DESCRIPTION:  Program which determines employee gross pay and outputs to the screen
  * and writes to file.
- * Allows for multiple entries and caulates overtime pay.
+ * Allows for multiple entries and caulates overtime pay and employee avrages.
  ****************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define STD_HOURS 40.0 // Standard weekely hours before overtime.
-// #define LOT_SIZE 5     // Default number of employee to enter into the system.
+#define LOT_SIZE 5     // Default number of employee to enter into the system.
 
 int main()
 {
-    int clock_number;       // employee clock number
-    int num_employees;
-    float gross;          // gross pay for week (wage * hours)
-    float hours;          // number of hours worked per week
-    float overtime_hours; // number of overtime hours worked
-    float wageRate;       // hourly wage
+    // employee clock number
+    int clock_number[LOT_SIZE] = {98401, 526488, 765349, 34645, 127615};
+    // hourly wage
+    float wageRate [LOT_SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35};    
+    float gross[LOT_SIZE];          // gross pay for week (wage * hours)
+    float hours[LOT_SIZE];          // number of hours worked per week
+    float overtime_hours[LOT_SIZE]; // number of overtime hours worked
+    float overtime_pay[LOT_SIZE];   // amount of overtim pay in dollars
+
+    float avg_data [6];             // array to store avrage of each caculation 
 
     FILE *output; // pointer to the output file
 
@@ -42,58 +46,58 @@ int main()
     fprintf(output, "Clock#\tWage\tHours\tOT Pay\t\tGross");
     fprintf(output, "\n---------------------------------------------------------------\n");
 
-    printf("\nHow many entries are you entering: ");
-    scanf("%d", &num_employees);
-
-    if (num_employees < 0)
-    {
-        printf("[ERROR] value cannot be negative\n");
-        return -1; // return -1 if an error occurs
-    }
 
     // start looping
-    for (size_t i = 0; i < num_employees; i++)
+    for (size_t i = 0; i < LOT_SIZE; i++)
     {
-        printf("\n***** Pay Caculator *****\n");
-        printf("\nEnter Your Employee ID: ");
-        scanf("%d", &clock_number);
+        // printf("\n***** Pay Caculator *****\n");
+        // printf("\nEnter Your Employee ID: ");
+        // scanf("%d", &clock_number[i]);
 
-        printf("\nEnter hourly wage: ");
-        scanf("%f", &wageRate);
+        // printf("\nEnter hourly wage: ");
+        // scanf("%f", &wageRate[i]);
 
-        printf("\nEnter hours worked: ");
-        scanf("%f", &hours);
-
-        // determine if employee worked overtime and cacualte overtime hours.
-        if (STD_HOURS < hours)
-        {
-            overtime_hours = hours - STD_HOURS;
-            printf("overtime_hours: %2.2f\n", overtime_hours);
-        }
-
-        if ((clock_number < 0) || (wageRate < 0) || (hours < 0) || (num_employees < 0) || (overtime_hours < 0))
+        printf("\n [ID: %06d] Enter hours worked: ", clock_number[i]);
+        scanf("%f", &hours[i]);
+        
+        // Cant work negitave hours.
+        if (hours[i] < 0)
         {
             printf("[ERROR] value cannot be negative entry terminated\n");
+            hours[i] = 0; 
             continue; // continue program although error has occured.
         }
 
-        // calculate gross pay with overtime
-        float overtime_pay = ((wageRate * 1.5) * overtime_hours);
-        gross = (wageRate * (hours - overtime_hours)) + overtime_pay;
+        // determine if employee worked overtime and cacualte overtime hours.
+        if (STD_HOURS < hours[i])
+        {
+            overtime_hours[i] = hours[i] - STD_HOURS;
+        }
 
-        // print to screen
-        printf("\n---------------------------------------------------------------\n");
-        printf("Clock#\tWage\tHours\tOT Hours\tOT Pay\t\tGross");
-        printf("\n---------------------------------------------------------------\n");
-        printf("%06d\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n", 
-            clock_number, wageRate, hours, overtime_hours, overtime_pay, gross);
+        // calculate gross pay with overtime
+        overtime_pay[i] = ((wageRate[i] * 1.5) * overtime_hours[i]);
+        gross[i] = (wageRate[i] * (hours[i] - overtime_hours[i])) + overtime_pay[i];
+
+    } // end for
+
+    // Print header
+    printf("\n---------------------------------------------------------------\n");
+    printf("Clock#\tWage\tHours\tOT Hours\tOT Pay\t\tGross");
+    printf("\n---------------------------------------------------------------\n");
+
+    // loop for prinint
+    for (size_t i = 0; i < LOT_SIZE; i++)
+    {
+       
+        printf("%06d\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+               clock_number[i], wageRate[i], hours[i], overtime_hours[i], overtime_pay[i], gross[i]);
 
         // write to file
-        fprintf(output, "%06d\t%2.2f\t%2.2f\t%2.2f\t+%2.2f\t\t%2.2f\n", 
-            clock_number, wageRate, hours, overtime_hours, overtime_pay, gross);
+        fprintf(output, "%06d\t%2.2f\t%2.2f\t%2.2f\t+%2.2f\t\t%2.2f\n",
+                clock_number[i], wageRate[i], hours[i], overtime_hours[i], overtime_pay[i], gross[i]);
 
-        overtime_hours = 0; // reset for next entry
     } // end for
+    
     // close the file
     fclose(output);
     return 0;
