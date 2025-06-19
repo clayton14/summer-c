e4d0f1eb3e4fecec48243e05e77babb1b3e70ddb
clayton14 <claytoneasley77@gmail.com>
2025-06-18 22:40:14 -0400

    Assignment 4 Changes
      Assignment 3 and 4 are in week 4 folder

--- week4/src/main.c
+++ week4/src/main.c
@@ -17,19 +17,20 @@
 
 #define STD_HOURS 40.0 // Standard weekely hours before overtime.
 #define LOT_SIZE 5     // Default number of employee to enter into the system.
+#define STD_OVERTIME 1.5
 
 int main()
 {
     // employee clock number
     int clock_number[LOT_SIZE] = {98401, 526488, 765349, 34645, 127615};
     // hourly wage
-    float wageRate [LOT_SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35};    
-    float gross[LOT_SIZE];          // gross pay for week (wage * hours)
-    float hours[LOT_SIZE];          // number of hours worked per week
+    float wageRate[LOT_SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35};
+    float hours[LOT_SIZE];                    // number of hours worked per week
     float overtime_hours[LOT_SIZE]; // number of overtime hours worked
     float overtime_pay[LOT_SIZE];   // amount of overtim pay in dollars
+    float gross[LOT_SIZE];                    // gross pay for week (wage * hours)
 
-    float avg_data [6];             // array to store avrage of each caculation 
+    float avg_data[5] = {0}; // array to store avrage of each metric
 
     FILE *output; // pointer to the output file
 
@@ -46,25 +47,17 @@ int main()
     fprintf(output, "Clock#\tWage\tHours\tOT Pay\t\tGross");
     fprintf(output, "\n---------------------------------------------------------------\n");
 
-
     // start looping
     for (size_t i = 0; i < LOT_SIZE; i++)
     {
-        // printf("\n***** Pay Caculator *****\n");
-        // printf("\nEnter Your Employee ID: ");
-        // scanf("%d", &clock_number[i]);
-
-        // printf("\nEnter hourly wage: ");
-        // scanf("%f", &wageRate[i]);
-
         printf("\n [ID: %06d] Enter hours worked: ", clock_number[i]);
         scanf("%f", &hours[i]);
-        
+
         // Cant work negitave hours.
         if (hours[i] < 0)
         {
             printf("[ERROR] value cannot be negative entry terminated\n");
-            hours[i] = 0; 
+            hours[i] = 0;
             continue; // continue program although error has occured.
         }
 
@@ -73,22 +66,35 @@ int main()
         {
             overtime_hours[i] = hours[i] - STD_HOURS;
         }
+        else
+        {
+            overtime_hours[i] = 0;
+        }
 
         // calculate gross pay with overtime
-        overtime_pay[i] = ((wageRate[i] * 1.5) * overtime_hours[i]);
+        overtime_pay[i] = ((wageRate[i] * STD_OVERTIME) * overtime_hours[i]);
         gross[i] = (wageRate[i] * (hours[i] - overtime_hours[i])) + overtime_pay[i];
 
+        // sum total of each metric in there respective index in the array
+        // defined in order as listed above
+        avg_data[0] += wageRate[i];
+        avg_data[1] += hours[i];
+        avg_data[2] += overtime_hours[i];
+        avg_data[3] += overtime_pay[i];
+        avg_data[4] += gross[i];
+
     } // end for
+    
 
     // Print header
     printf("\n---------------------------------------------------------------\n");
     printf("Clock#\tWage\tHours\tOT Hours\tOT Pay\t\tGross");
     printf("\n---------------------------------------------------------------\n");
 
-    // loop for prinint
+    // loop
     for (size_t i = 0; i < LOT_SIZE; i++)
     {
-       
+        // print to stdout
         printf("%06d\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
                clock_number[i], wageRate[i], hours[i], overtime_hours[i], overtime_pay[i], gross[i]);
 
@@ -97,7 +103,25 @@ int main()
                 clock_number[i], wageRate[i], hours[i], overtime_hours[i], overtime_pay[i], gross[i]);
 
     } // end for
-    
+
+    // Spacer
+    printf("---------------------------------------------------------------\n");
+
+    // print avrages using format described in assignment 4
+    // display and write total
+    printf("Total\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+           avg_data[0], avg_data[1], avg_data[2], avg_data[3], avg_data[4]);
+
+    fprintf(output, "Total\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+            avg_data[0], avg_data[1], avg_data[2], avg_data[3], avg_data[4]);
+
+    // display avg
+    printf("Avrage \t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+           avg_data[0] / LOT_SIZE, avg_data[1] / LOT_SIZE, avg_data[2] / LOT_SIZE, avg_data[3] / LOT_SIZE, avg_data[4] / LOT_SIZE);
+
+    fprintf(output, "Total\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+            avg_data[0] / LOT_SIZE, avg_data[1] / LOT_SIZE, avg_data[2] / LOT_SIZE, avg_data[3] / LOT_SIZE, avg_data[4] / LOT_SIZE);
+
     // close the file
     fclose(output);
     return 0;
