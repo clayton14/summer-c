83e1bff04725ec2f1fdd4594544dc904e1beabfc
clayton14 <claytoneasley77@gmail.com>
2025-06-29 17:30:43 -0400

    Add function implementations

--- week6/src/employee.c
+++ week6/src/employee.c
@@ -8,10 +8,10 @@
  * DATE:        <2025-06-24>
  *
  * DESCRIPTION:  Implimatation for employee
- * 
+ *
  * determines employee gross pay and outputs to the screen
- * and writes to file. 
- * Allows for multiple entries and caulates overtime pay 
+ * and writes to file.
+ * Allows for multiple entries and caulates overtime pay
  * and employee avrages
  ****************************************/
 
@@ -32,7 +32,7 @@ bool is_overtime(int hours_worked)
     }
 }
 
-float caculate_pay(float hours, float wage_rate)
+float caculate_gross(float hours, float wage_rate)
 {
     float gross;
     if (is_overtime(hours))
@@ -49,44 +49,49 @@ float caculate_pay(float hours, float wage_rate)
     return gross;
 }
 
-void log_row(char *filename, int clock_number, float *row)
+float caculate_ot_pay(float hours, float wage_rate)
 {
+    return ((wage_rate * STD_OVERTIME) * hours - STD_HOURS);
+}
 
-    // int clock_number, float wage_rate, float hours, float overtime_hours, float overtime_pay, float gross
-    //  pointer to the output file
+float caculate_avg(float *arr)
+{
+    int len = sizeof(arr) / sizeof(arr[0]);
+    float avg;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        avg += arr[i];
+    }
+
+    return avg / len;
+}
+
+void log_emp(char *filename, int clock_number, float wage_rate, float hours, float overtime_hours, float overtime_pay, float gross)
+{
     FILE *output;
-    // Open file in append mode
     if ((output = fopen(filename, "a")) == (FILE *)NULL)
     {
         fprintf(stderr, "[ERROR] Unable to open file employee.log\n");
         return -1;
     }
-    // write to file
     fprintf(output, "%06d\t%2.2f\t%2.2f\t%2.2f\t+%2.2f\t\t%2.2f\n",
-            row[0], row[1], row[2], row[3], row[4], row[5]);
+            clock_number, wage_rate, hours, overtime_hours, overtime_pay, gross);
 
     fclose(output);
 }
 
-/**
- * Function: display_output()
- * ---------------------------
- *  Displays table of employee data to stdout
- *  @param num_entries the number of entries to display to stdout
- **/
-void display_row(int clock_number, float *row)
-
+void print_emp(int clock_number, float wage_rate, float hours, float overtime_hours, float overtime_pay, float gross)
 {
+    printf("%06d\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+           clock_number, wage_rate, hours, overtime_hours, overtime_pay, gross);
+}
 
-    // print header
-    printf("\n---------------------------------------------------------------\n");
-    printf("Clock#\tWage\tHours\tOT Hours\tOT Pay\t\tGross");
-    printf("\n---------------------------------------------------------------\n");
-
+void display_row(float *row)
+{
     for (size_t i = 0; i < LOT_SIZE; i++)
     {
-
-        printf("%06d\t%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
-               clock_number, row[0], row[1], row[2], row[3], row[4], row[5]);
+        printf("%2.2f\t%2.2f\t%2.2f\t\t+%2.2f\t\t%2.2f\n",
+               row[0], row[1], row[2], row[3], row[4], row[5]);
     } // end for
 }
